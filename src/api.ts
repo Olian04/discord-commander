6d9888c9f72396d8dfb6d496164a847d55e8c119
Oliver <rift95@live.se>
2019-11-01 14:42:55 +0100

    exporting LogLevels

--- src/api.ts
+++ src/api.ts
@@ -3,3 +3,4 @@ export { Commander } from './lib/commander';
 export { subscribe } from './lib/decorators/subscribe';
 export { parse } from './lib/decorators/parse';
 export { Event } from './lib/event';
+export { LogLevels } from './lib/util/logging';
